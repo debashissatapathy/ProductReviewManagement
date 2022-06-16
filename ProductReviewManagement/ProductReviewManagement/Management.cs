a5371d6095938bde1faef532f0f51217716d08c3
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-16 18:29:17 +0530

    Fetch top 3 record from product review list

---
+++ ProductReviewManagement/ProductReviewManagement/Management.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public readonly DataTable datatable = new DataTable();
+
+        public void TopRecord(List<ProductReview> listreview)
+        {
+            var recordData = (from productReviews in listreview
+                              orderby productReviews.Rating descending
+                              select productReviews).Take(3);
+            foreach(var list in recordData)
+            {
+                Console.WriteLine("ProductId: {0}, UserId: {1}, Rating: {2}, Review: {3}, IsLike: {4}",
+                        list.ProductId, list.UserId, list.Rating, list.Review, list.IsLike);
+            }
+        }
+       
+
+    }
+}
