2ebf58bd51e4bb213c020f5544a339e288e7fd61
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-17 15:16:48 +0530

    Retrieve UserId10 orderby Rating

--- ProductReviewManagement/ProductReviewManagement/Management.cs
+++ ProductReviewManagement/ProductReviewManagement/Management.cs
@@ -10,7 +10,7 @@ namespace ProductReviewManagement
     //UC2: Fetch top 3 records from ProductReview list
     public class Management
     {
-        public readonly DataTable datatable = new DataTable();
+        //public readonly DataTable dataTable = new DataTable();
 
         public void TopRecord(List<ProductReview> listreview)
         {
@@ -23,9 +23,9 @@ namespace ProductReviewManagement
                         list.ProductId, list.UserId, list.Rating, list.Review, list.IsLike);
             }
         }
-        //UC3: Management - Retrieve all record from the list who’s rating are greater then 3 and productID is 1 or 4 or 9
+        //UC3: Retrieve all record from the list who’s rating are greater then 3 and productID is 1 or 4 or 9
        public void SelectedRecord(List<ProductReview> listreview)
-        {
+       {
             var recordData = (from productReviews in listreview
                               where (productReviews.ProductId == 1 || productReviews.ProductId == 4 || productReviews.ProductId == 9)
                                      && (productReviews.Rating > 3)                                     
@@ -36,13 +36,132 @@ namespace ProductReviewManagement
                         list.ProductId, list.UserId, list.Rating, list.Review, list.IsLike);
             }
         }
-        //UC4: Management - Retrieve count of review present for each productID
+        //UC4: Retrieve count of review present for each productID
         public void CountOfRecord(List<ProductReview> listreview)
         {
             var recordData = listreview.GroupBy(x => x.ProductId).Select(x => new { ProductId = x.Key, Count = x.Count() });
             foreach(var list in recordData)
             {
                 Console.WriteLine(list.ProductId + ":: " + list.Count);
+            }
+        }
+        //UC5: Retrieve only productId and review from the list for all records.
+        public void Retrieve_ProductId_Review(List<ProductReview> listreview)
+        {
+            var recordData =  listreview.Select (x => new {productId = x.ProductId, productReview = x.Review });
+            foreach( var list in recordData)
+            {
+                Console.WriteLine("ProductId: {0},  Review: {1}", list.productId, list.productReview);
+            }
+        }
+        //UC6: Skip top 5 records from the list
+        public void SkipTop5Records(List<ProductReview> listreview)
+        {
+            var recordData = (from productReviews in listreview
+                              select productReviews).Skip(5);
+            foreach( var list in recordData)
+            {
+                Console.WriteLine("ProductId: {0}, UserId: {1}, Rating: {2}, Review: {3}, IsLike: {4}",
+                        list.ProductId, list.UserId, list.Rating, list.Review, list.IsLike);
+            }    
+        }
+        //UC8: Create DataTable using C# and Add ProductID, UserID, Rating, Review and isLike fields in that.
+                //Add 25 default values in datatable list which we have UC created 8
+
+        public void CreateDataTable()
+        {
+            //Create datatable
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("ProductID");
+            dataTable.Columns.Add("UserID");
+            dataTable.Columns.Add("Rating");
+            dataTable.Columns.Add("Review");
+            dataTable.Columns.Add("isLike");
+
+            //Create row
+            dataTable.Rows.Add(1, 1, 2, "Good", true);
+            dataTable.Rows.Add(2, 2, 4, "Good", true);
+            dataTable.Rows.Add(3, 3, 1, "Bad",  true);
+            dataTable.Rows.Add(4, 4, 5, "Good", false);
+            dataTable.Rows.Add(5, 5, 1, "Bad", false);
+            dataTable.Rows.Add(6, 6, 5, "Nice",true);
+            dataTable.Rows.Add(7, 7, 5, "Nice", true);
+            dataTable.Rows.Add(8, 8, 4 , "Good", false);
+            dataTable.Rows.Add(9, 9, 2, "Bad", false);
+            dataTable.Rows.Add(10, 10, 3, "Good", true); 
+            dataTable.Rows.Add(11, 11, 4, "Good", true);
+            dataTable.Rows.Add(12 , 12, 1, "Bad", true);
+            dataTable.Rows.Add(13, 13, 5, "Nice", true);
+            dataTable.Rows.Add(14, 14, 1, "Bad", true);
+            dataTable.Rows.Add(15, 15,5, "Good", false);
+            dataTable.Rows.Add(16, 16,1, "Bad", false);
+            dataTable.Rows.Add(17, 17,5, "Nice", true);
+            dataTable.Rows.Add(18, 18,5, "Nice", true);
+            dataTable.Rows.Add(19, 19,4, "Good", false);
+            dataTable.Rows.Add(20, 20,2, "Bad", false);
+            dataTable.Rows.Add(21, 21,3, "Good", true);
+            dataTable.Rows.Add(22, 22,4, "Good", true);
+            dataTable.Rows.Add(23, 23,1, "Bad", true);
+            dataTable.Rows.Add(24, 24,5, "Nice", true);
+            dataTable.Rows.Add(25, 25,4, "Good", false);
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("***********All Data from the dataTable************");
+            Console.ResetColor();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine($"{row["ProductId"]}\t|{row["UserId"]}\t|{row["Review"]}\t|{row["Rating"]}\t|{row["Islike"]}");
+            }
+        }
+        //UC9: Retrieve all the records from the datatable variable who’s isLike value is true
+        public void Retrieve_IsLike_True(List<ProductReview> listreview)
+        {
+            var recordData = (from productReviews in listreview
+                              where (productReviews.IsLike == true)
+                              select productReviews);
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductId: {0}, UserId: {1}, Rating: {2}, Review: {3}, IsLike: {4}",
+                        list.ProductId, list.UserId, list.Rating, list.Review, list.IsLike);
+            }
+        }
+        //UC10: Find average rating of the each productId
+
+        public void AvaregeRating(List<ProductReview> listreview)
+        {
+            var recordData = listreview.GroupBy(x => x.ProductId).Select(x => new
+            {
+                product = x.Key,
+                Count = x.Average(a => a.Rating)
+            });
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("Product Id:{0} => Average Rating :{1}", list.product, list.Count);
+            }
+        }
+        //UC11: Retreive all records from the list who’s review message contains “nice”
+        public void Review_Contains_Nice(List<ProductReview> listreview)
+        {
+            var recordData = (from productReviews in listreview
+                              where productReviews.Review == "Nice"
+                              select productReviews);
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductId: {0}, UserId: {1}, Rating: {2}, Review: {3}, IsLike: {4}",
+                                     list.ProductId, list.UserId, list.Rating, list.Review, list.IsLike);
+            }
+        }
+        //UC12: all records from the list who’s Userid = 10 and order by rating
+        public void Retieve_UserId10(List<ProductReview> listview)
+        {
+            var recordData = (from productreviews in listview
+                              where productreviews.UserId == 10
+                              orderby productreviews.Rating
+                              select productreviews);
+            foreach(var list in recordData)
+            {
+                Console.WriteLine("ProductId: {0}, UserId: {1}, Rating: {2}, Review: {3}, IsLike: {4}",
+                                    list.ProductId, list.UserId, list.Rating, list.Review, list.IsLike);
             }
         }
     }
--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -1,46 +1,136 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ProductReviewManagement;
 
-    Console.WriteLine("Welcome to Product review Management Problem!");
-    //UC1: 25 default value added in list
-    List<ProductReview> productReviewList = new List<ProductReview>()
+Console.WriteLine("Welcome to Product review Management Problem!");
+//UC1: 25 default value added in list
+List<ProductReview> productReviewList = new List<ProductReview>()
+{
+    new ProductReview(){ProductId = 1, UserId = 1, Rating = 2, Review = "Good", IsLike = true},
+    new ProductReview(){ProductId = 2, UserId = 2, Rating = 4, Review = "Good", IsLike = true},
+    new ProductReview(){ProductId = 3, UserId = 3, Rating = 3, Review = "Bad", IsLike= true},
+    new ProductReview(){ProductId = 4, UserId = 4, Rating = 4.5, Review = "Good", IsLike = false},
+    new ProductReview(){ProductId = 5, UserId = 5, Rating = 8, Review = "Bad", IsLike = false },
+    new ProductReview(){ProductId = 6, UserId = 6, Rating = 7, Review = "Nice", IsLike = true},
+    new ProductReview(){ProductId = 7, UserId = 7, Rating = 5, Review = "Nice", IsLike = true},
+    new ProductReview(){ProductId = 8, UserId = 8, Rating = 9, Review = "Good", IsLike = false},
+    new ProductReview(){ProductId = 9, UserId = 9, Rating = 3, Review = "Bad", IsLike = false},
+    new ProductReview(){ProductId = 10, UserId = 10, Rating = 6.5, Review = "Nice", IsLike = true},
+    new ProductReview(){ProductId = 11, UserId = 11, Rating = 8, Review = "Nice", IsLike = true},
+    new ProductReview(){ProductId = 12, UserId = 12, Rating = 1, Review = "Nice", IsLike = true},
+    new ProductReview(){ProductId = 13, UserId = 13, Rating = 3.5, Review = "Nice", IsLike = true},
+    new ProductReview(){ProductId = 14, UserId = 14, Rating = 7.5, Review = "Good", IsLike = true},
+    new ProductReview(){ProductId = 15, UserId = 15, Rating = 2, Review = "Bad", IsLike = false},
+    new ProductReview(){ProductId = 16, UserId = 16, Rating = 5, Review = "Nice", IsLike = true},
+    new ProductReview(){ProductId = 17, UserId = 17, Rating = 4, Review = "Nice", IsLike = true},
+    new ProductReview(){ProductId = 18, UserId = 18, Rating = 5, Review = "Good", IsLike = true},
+    new ProductReview(){ProductId = 19, UserId = 19, Rating = 5, Review = "Good", IsLike = true},
+    new ProductReview(){ProductId = 20, UserId = 20, Rating = 5, Review = "Good", IsLike = true},
+    new ProductReview(){ProductId = 21, UserId = 21, Rating = 5, Review = "Good", IsLike = true},
+    new ProductReview(){ProductId = 22, UserId = 22, Rating = 5, Review = "Good", IsLike = true},
+    new ProductReview(){ProductId = 23, UserId = 23, Rating = 5, Review = "Good", IsLike = false},
+    new ProductReview(){ProductId = 24, UserId = 24, Rating = 5, Review = "Good", IsLike = true},
+    new ProductReview(){ProductId = 25, UserId = 25, Rating = 5, Review = "Good", IsLike = true},
+};
+
+Console.WriteLine("Choose an option to take action: \n1.UC1: Add 25 default value\n2.//UC2: Fetch top 3 records" +
+                    "\n3.UC3: record who’s rating > 3 and productID is 1 or 4 or 9\n4.UC4: count of review ineach productID" +
+                    "\n5.UC5: Retrieve productId and review\n6.UC6: Skip top 5 records\n7.UC7: Retrieve productId and review" +
+                    "\n8.Create Datatable\n9. fetch who's review is true\n10.Avarege Rating\n11.review message contains “nice”" +
+                    "\n12.Retrieve Userid = 10 and order by rating");
+int option = Convert.ToInt16(Console.ReadLine());
+Management management = new Management();
+while (true)
+{
+    switch (option)
     {
-        new ProductReview(){ProductId = 1, UserId = 1, Rating = 2, Review = "Good", IsLike = true},
-        new ProductReview(){ProductId = 2, UserId = 2, Rating = 4, Review = "Good", IsLike = true},
-        new ProductReview(){ProductId = 3, UserId = 3, Rating = 3, Review = "Bad", IsLike= true},
-        new ProductReview(){ProductId = 4, UserId = 4, Rating = 4.5, Review = "Good", IsLike = false},
-        new ProductReview(){ProductId = 2, UserId = 5, Rating = 8, Review = "Bad", IsLike = false },
-        new ProductReview(){ProductId = 6, UserId = 6, Rating = 7, Review = "Nice", IsLike = true},
-        new ProductReview(){ProductId = 1, UserId = 7, Rating = 5, Review = "Nice", IsLike = true},
-        new ProductReview(){ProductId = 8, UserId = 8, Rating = 9, Review = "Good", IsLike = false},
-        new ProductReview(){ProductId = 9, UserId = 9, Rating = 3, Review = "Bad", IsLike = false},
-        new ProductReview(){ProductId = 10, UserId = 10, Rating = 6.5, Review = "Nice", IsLike = true},
-        new ProductReview(){ProductId = 11, UserId = 11, Rating = 8, Review = "Nice", IsLike = true},
-        new ProductReview(){ProductId = 1, UserId = 12, Rating = 1, Review = "Nice", IsLike = true},
-        new ProductReview(){ProductId = 13, UserId = 13, Rating = 3.5, Review = "Nice", IsLike = true},
-        new ProductReview(){ProductId = 14, UserId = 14, Rating = 7.5, Review = "Good", IsLike = true},
-        new ProductReview(){ProductId = 15, UserId = 15, Rating = 2, Review = "Bad", IsLike = false},
-        new ProductReview(){ProductId = 6, UserId = 16, Rating = 5, Review = "Nice", IsLike = true},
-        new ProductReview(){ProductId = 17, UserId = 17, Rating = 4, Review = "Nice", IsLike = true},
-        new ProductReview(){ProductId = 18, UserId = 18, Rating = 5, Review = "Good", IsLike = true},
-        new ProductReview(){ProductId = 9, UserId = 19, Rating = 5, Review = "Good", IsLike = true},
-        new ProductReview(){ProductId = 20, UserId = 20, Rating = 5, Review = "Good", IsLike = true},
-        new ProductReview(){ProductId = 21, UserId = 21, Rating = 5, Review = "Good", IsLike = true},
-        new ProductReview(){ProductId = 2, UserId = 22, Rating = 5, Review = "Good", IsLike = true},
-        new ProductReview(){ProductId = 23, UserId = 23, Rating = 5, Review = "Good", IsLike = false},
-        new ProductReview(){ProductId = 24, UserId = 24, Rating = 5, Review = "Good", IsLike = true},
-        new ProductReview(){ProductId = 25, UserId = 25, Rating = 5, Review = "Good", IsLike = true},
-    };
-    //foreach(var list in productReviewList)
-    //{
-    //    Console.WriteLine("ProductId: {0}, UserId: {1}, Rating: {2}, Review: {3}, IsLike: {4}",
-    //                        list.ProductId, list.UserId, list.Rating, list.Review, list.IsLike);
+        case 1:
+            foreach (var list in productReviewList)
+            {
+                Console.WriteLine("ProductId: {0}, UserId: {1}, Rating: {2}, Review: {3}, IsLike: {4}",
+                                    list.ProductId, list.UserId, list.Rating, list.Review, list.IsLike);
+            }
+            break;
+        case 2:
+            management.TopRecord(productReviewList);
+            break;
+        case 3:
+            management.SelectedRecord(productReviewList);
+            break;
+        case 4:
+            management.CountOfRecord(productReviewList);
+            break;
+        case 5:
+            management.Retrieve_ProductId_Review(productReviewList);
+            break;
+        case 6:
+            management.SkipTop5Records(productReviewList);
+            break;
+        case 7:
+            management.Retrieve_ProductId_Review(productReviewList);
+            break;
+        case 8:
+            management.CreateDataTable();
+            break;
+        case 9:
+            management.Retrieve_IsLike_True(productReviewList);
+            break;
+        case 10:
+
+
+            List<ProductReview> productList = new List<ProductReview>();
+
+            productList.Add(new ProductReview() { ProductId = 1, UserId = 1, Rating = 2, Review = "Good", IsLike = true });
+            productList.Add(new ProductReview() { ProductId = 1, UserId = 2, Rating = 4, Review = "Good", IsLike = true });
+            productList.Add(new ProductReview() { ProductId = 2, UserId = 3, Rating = 3, Review = "Bad", IsLike = true });
+            productList.Add(new ProductReview() { ProductId = 2, UserId = 4, Rating = 4.5, Review = "Good", IsLike = false });
+            productList.Add(new ProductReview() { ProductId = 3, UserId = 5, Rating = 8, Review = "Bad", IsLike = false });
+            productList.Add(new ProductReview() { ProductId = 3, UserId = 6, Rating = 7, Review = "Nice", IsLike = true });
+            productList.Add(new ProductReview() { ProductId = 4, UserId = 7, Rating = 5, Review = "Nice", IsLike = true });
+            productList.Add(new ProductReview() { ProductId = 4, UserId = 8, Rating = 9, Review = "Good", IsLike = false });
+            productList.Add(new ProductReview() { ProductId = 5, UserId = 9, Rating = 3, Review = "Bad", IsLike = false });
+            productList.Add(new ProductReview() { ProductId = 5, UserId = 10, Rating = 6.5, Review = "Nice", IsLike = true });
+            productList.Add(new ProductReview() { ProductId = 6, UserId = 11, Rating = 8, Review = "Nice", IsLike = true });
+            productList.Add(new ProductReview() { ProductId = 6, UserId = 12, Rating = 1, Review = "Nice", IsLike = true });
+            productList.Add(new ProductReview() { ProductId = 7, UserId = 13, Rating = 3.5, Review = "Nice", IsLike = true });
+            productList.Add(new ProductReview() { ProductId = 7, UserId = 14, Rating = 7.5, Review = "Good", IsLike = true });
+            management.AvaregeRating(productList);
+            break;
+        case 11:
+            management.Review_Contains_Nice(productReviewList);
+            break;
+        case 12:
+            List<ProductReview> reviewList = new List<ProductReview>();
+
+            reviewList.Add(new ProductReview() { ProductId = 1, UserId = 1, Rating = 2, Review = "Good", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 2, UserId = 2, Rating = 4, Review = "Good", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 3, UserId = 3, Rating = 3, Review = "Bad", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 4, UserId = 4, Rating = 4.5, Review = "Good", IsLike = false });
+            reviewList.Add(new ProductReview() { ProductId = 5, UserId = 5, Rating = 8, Review = "Bad", IsLike = false });
+            reviewList.Add(new ProductReview() { ProductId = 6, UserId = 6, Rating = 7, Review = "Nice", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 7, UserId = 7, Rating = 5, Review = "Nice", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 8, UserId = 8, Rating = 9, Review = "Good", IsLike = false });
+            reviewList.Add(new ProductReview() { ProductId = 9, UserId = 9, Rating = 3, Review = "Bad", IsLike = false });
+            reviewList.Add(new ProductReview() { ProductId = 10, UserId = 10, Rating = 5, Review = "Nice", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 10, UserId = 10, Rating = 6, Review = "Bad", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 10, UserId = 10, Rating = 9, Review = "Good", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 10, UserId = 10, Rating = 4, Review = "Nice", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 10, UserId = 10, Rating = 3, Review = "Bad", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 10, UserId = 10, Rating = 1, Review = "Nice", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 11, UserId = 11, Rating = 8, Review = "Good", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 12, UserId = 12, Rating = 1, Review = "Nice", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 13, UserId = 13, Rating = 3.5, Review = "Nice", IsLike = true });
+            reviewList.Add(new ProductReview() { ProductId = 14, UserId = 14, Rating = 7.5, Review = "Good", IsLike = true });
+            
+            management.Retieve_UserId10(reviewList);
+            break;
+
+
+    }        
+
+            
     
-    //}
-    //UC2
-    Management management = new Management();
-    //management.TopRecord(productReviewList);
-    //UC3
-    //management.SelectedRecord(productReviewList);
-    //UC4
-    management.CountOfRecord(productReviewList);
+}
+    
+    
+    
+
